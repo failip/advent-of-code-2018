adafcbf53538d0be3212b154d05c9fc8f0c86582
failip <chickenguy999@gmail.com>
2018-12-05 02:06:57 +0100

    Cleaned up files

--- 1/chronal-calibration.py
+++ 1/chronal-calibration.py
@@ -1,28 +1,21 @@
-def part_one():
-    f = open('input', 'r')
-    s = 0
+f = open('input', 'r')
+s = 0
+for num in f:
+    s += int(num)
+print(s)
+
+f.seek(0)
+found_duplicate = False
+current_freq = 0
+reached_freq = {}
+while (not found_duplicate):
     for num in f:
-        s += int(num)
-    f.close()
-    print(s)
-
-part_one()
-
-def part_two():
-    f = open('input', 'r')
-    found_duplicate = False
-    current_freq = 0
-    reached_freq = [0]
-    while (not found_duplicate):
-        for num in f:
-            current_freq += int(num)
-            if (current_freq in reached_freq):
-                print(current_freq)
-                found_duplicate = True
-                break
-            else:
-                reached_freq.append(current_freq)
-        f.seek(0)
-    f.close()
-        
-part_two()
+        current_freq += int(num)
+        if (current_freq in reached_freq):
+            print(current_freq)
+            found_duplicate = True
+            break
+        else:
+            reached_freq[current_freq] = 0
+    f.seek(0)
+f.close()
