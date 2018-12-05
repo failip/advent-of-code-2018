08c4d1930e50ba35a193f74e205a82972485fa90
failip <chickenguy999@gmail.com>
2018-12-05 01:33:08 +0100

    Finished day4 part one

--- 4/repose-report.py
+++ 4/repose-report.py
@@ -1,2 +1,59 @@
-def sort_file(f):
+#from subprocess import call
+import numpy as np
+
+class Guard:
+    sleep = 0
+    def __init__(self, ID):
+        self.ID = ID
+        self.minutes_slept = 0
+        self.minutes = np.zeros(60)
+
+    def fall_asleep(self, time):
+        self.sleep = time
     
+    def wake_up(self, time):
+        for i in range(self.sleep, time):
+            self.minutes_slept += 1
+            self.minutes[i] += 1
+        self.sleep = 0
+
+#call("sort input -o sorted", shell = True) open('sorted','r')
+f = open('sorted', 'r')
+guards = {}
+for line in f:
+    if 'Guard' in line:
+        splitted = line.split(' ')
+        ID = int(splitted[3][1:])
+        guard = guards.get(ID)
+        if(not guard):
+            guard = Guard(ID)
+            guards[ID] = guard
+    else:
+        splitted = line.split(' ')
+        time = int(splitted[1][3:][:-1])
+        if 'falls' in line:
+            guard.fall_asleep(time)
+        else:
+            guard.wake_up(time)
+
+max_time_asleep = 0
+guard_longest_asleep = None
+for key in guards:
+    guard = guards[key]
+    if guard.minutes_slept > max_time_asleep:
+        max_time_asleep = guard.minutes_slept
+        guard_longest_asleep = guard
+
+print(f'Guard #{guard_longest_asleep.ID} was asleep for {guard_longest_asleep.minutes_slept} Minutes.')
+print(f'Most time asleep at Minute {guard_longest_asleep.minutes.argmax()}.')
+print(f'Solution: {guard_longest_asleep.minutes.argmax() * guard_longest_asleep.ID}')
+    
+        
+    
+
+
+    
+    
+
+
+
