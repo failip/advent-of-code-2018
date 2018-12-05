5f0456a94a1d4bd116a2e18f6c6d750486f42558
failip <chickenguy999@gmail.com>
2018-12-05 01:46:55 +0100

    Finished day 4 part two

--- 4/repose-report.py
+++ 4/repose-report.py
@@ -1,4 +1,4 @@
-#from subprocess import call
+from subprocess import call
 import numpy as np
 
 class Guard:
@@ -6,6 +6,7 @@ class Guard:
     def __init__(self, ID):
         self.ID = ID
         self.minutes_slept = 0
+        self.most_slept_minute = 0
         self.minutes = np.zeros(60)
 
     def fall_asleep(self, time):
@@ -15,9 +16,10 @@ class Guard:
         for i in range(self.sleep, time):
             self.minutes_slept += 1
             self.minutes[i] += 1
-        self.sleep = 0
+        self.most_slept_minute = self.minutes.max()
 
-#call("sort input -o sorted", shell = True) open('sorted','r')
+call("sort input -o sorted", shell = True) 
+open('sorted','r')
 f = open('sorted', 'r')
 guards = {}
 for line in f:
@@ -44,12 +46,22 @@ for key in guards:
         max_time_asleep = guard.minutes_slept
         guard_longest_asleep = guard
 
+print('Strategy 1:')
 print(f'Guard #{guard_longest_asleep.ID} was asleep for {guard_longest_asleep.minutes_slept} Minutes.')
 print(f'Most time asleep at Minute {guard_longest_asleep.minutes.argmax()}.')
 print(f'Solution: {guard_longest_asleep.minutes.argmax() * guard_longest_asleep.ID}')
     
-        
-    
+max_time_asleep = 0
+guard_longest_asleep = None
+for key in guards:
+    guard = guards[key]
+    if guard.most_slept_minute > max_time_asleep:
+        max_time_asleep = guard.most_slept_minute
+        guard_longest_asleep = guard
+
+print('\nStrategy 2:')
+print(f'Guard #{guard_longest_asleep.ID} was asleep for {int(guard_longest_asleep.most_slept_minute)} Minutes on Minute {guard_longest_asleep.minutes.argmax()}.')
+print(f'Solution: {guard_longest_asleep.minutes.argmax() * guard_longest_asleep.ID}')
 
 
     
