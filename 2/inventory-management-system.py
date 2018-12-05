c9ca2a275f0d186991ecd440313600f8ba8b4bef
failip <chickenguy999@gmail.com>
2018-12-05 02:07:45 +0100

    Cleaned up files

--- 2/inventory-management-system.py
+++ 2/inventory-management-system.py
@@ -1,41 +1,34 @@
-def part_one():
-    f = open('input', 'r')
-    twice = 0
-    thrice = 0
-    for ID in f:
-        ID = ID.rstrip('\r\n')
-        letters = list(set(ID))
-        for l in letters:
-            if (ID.count(l) == 2):
-                twice += 1
-                break
-        for l in letters:
-            if (ID.count(l) == 3):
-                thrice += 1
-                break
-    f.close()
-    print(f'Twice: {twice}, Thrice: {thrice}, Checksum: {twice*thrice}')
-
-part_one()
-
-def part_two():
-    f = open('input', 'r')
-    length = len('wlpiogsvdfecjdqmnxakudrhbz')
-    found_correct = False
-    for ID in f:
-        f2 = open('input', 'r')
-        for ID2 in f2:
-            difference_positions = []
-            for i in range(length):   
-                if ID[i] != ID2[i]:
-                    difference_positions.append(i)
-            if len(difference_positions) == 1:
-                found_correct = True
-                break
-        if found_correct:
-            print(f'1st ID: {ID[:-1]}')
-            print(f'2nd ID: {ID2[:-1]}')
-            print(f'Without Difference: {(ID[:difference_positions[0]] + ID[difference_positions[0]+1:])[:-1]}')
+f = open('input', 'r')
+twice = 0
+thrice = 0
+for ID in f:
+    ID = ID.rstrip('\r\n')
+    letters = list(set(ID))
+    for l in letters:
+        if (ID.count(l) == 2):
+            twice += 1
             break
-        
-part_two()
+    for l in letters:
+        if (ID.count(l) == 3):
+            thrice += 1
+            break
+print(f'Twice: {twice}, Thrice: {thrice}, Checksum: {twice*thrice}')
+
+f.seek(0)
+length = len('wlpiogsvdfecjdqmnxakudrhbz')
+found_correct = False
+for ID in f:
+    f2 = open('input', 'r')
+    for ID2 in f2:
+        difference_positions = []
+        for i in range(length):   
+            if ID[i] != ID2[i]:
+                difference_positions.append(i)
+        if len(difference_positions) == 1:
+            found_correct = True
+            break
+    if found_correct:
+        print(f'1st ID: {ID[:-1]}')
+        print(f'2nd ID: {ID2[:-1]}')
+        print(f'Without Difference: {(ID[:difference_positions[0]] + ID[difference_positions[0]+1:])[:-1]}')
+        break
