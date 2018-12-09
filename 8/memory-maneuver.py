5c122bb5813f0bf69aa3d0473f52c4869d66702d
failip <chickenguy999@gmail.com>
2018-12-09 22:26:25 +0100

    Changed method names

--- 8/memory-maneuver.py
+++ 8/memory-maneuver.py
@@ -15,20 +15,20 @@ def create_node(tree):
     tree = tree[number_of_metadata_entries:]
     return tree, node
 
-def count_metadata_1(node):
+def count_metadata(node):
     metadata_sum = 0
     metadata_sum += sum(node.metadata)
     for i in range(len(node.children)):
-        metadata_sum += count_metadata_1(node.children[i])
+        metadata_sum += count_metadata(node.children[i])
     return metadata_sum
 
-def count_metadata_2(node):
+def calculate_value(node):
     metadata_sum = 0
     number_of_children = len(node.children)
     if node.children:
         for entry in node.metadata:
             if entry <= number_of_children:
-                metadata_sum += count_metadata_2(node.children[entry-1])
+                metadata_sum += calculate_value(node.children[entry-1])
     else:
         metadata_sum += sum(node.metadata)
     return metadata_sum
@@ -39,10 +39,10 @@ tree = tree.split()
 tree = list(map(int,tree))
 tree, root = create_node(tree)
 
-metadata_sum_1 = count_metadata_1(root)
+metadata_sum_1 = count_metadata(root)
 print('Part One:')
 print(f'Sum of metadata {metadata_sum_1}.')
 
-metadata_sum_2 = count_metadata_2(root)
+metadata_sum_2 = calculate_value(root)
 print('Part Two:')
 print(f'Sum of metadata {metadata_sum_2}.')
